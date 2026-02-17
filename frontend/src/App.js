f94ae419365dbc8c690291c2a56326947389e62e
Shubham Aggarwal <shubham.agg2795@gmail.com>
2026-02-17 17:04:54 +0530

    Comitted Changes related to s3 & Cognito configuration along with cloudfront distribution

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAuth } from "react-oidc-context";
 import Header from "./components/Header";
 import Login from "./pages/Login";
 import Search from "./pages/Search";
 import Product from "./pages/Product";
 
 function App() {
+  const auth = useAuth();
+
+  if (auth.isLoading) {
+    return <div>Loading authentication...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Error: {auth.error.message}</div>;
+  }
+
+  // If not authenticated → redirect to Cognito
+  if (!auth.isAuthenticated) {
+    auth.signinRedirect();
+    return <div>Redirecting to login...</div>;
+  }
+
+  // If authenticated → show app
   return (
     <BrowserRouter>
       <Header />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
+import { WebStorageStateStore } from "oidc-client-ts";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const cognitoAuthConfig = {
+  authority:
+    "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_w11q1SuUr",
+  client_id: "5itpocjgtbnrbktndqpqpn1ivm",
+  redirect_uri: "https://d1koynn5e9sgkm.cloudfront.net",
+  post_logout_redirect_uri: "https://d1koynn5e9sgkm.cloudfront.net/",
+  response_type: "code",
+  scope: "openid email phone",
+
+  userStore: new WebStorageStateStore({
+    store: window.localStorage,
+  }),
+
+  // 🔥 THIS FIXES REFRESH ISSUE
+  onSigninCallback: () => {
+    window.history.replaceState(
+      {},
+      document.title,
+      window.location.pathname
+    );
+  },
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
