45404f402bc9cdf76f1dcf92c9eefa6e725060af
Shubham Aggarwal <shubham.agg2795@gmail.com>
2026-02-26 20:08:28 +0530

    new UI change v1

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div style={{ padding: "12px", background: "#222", color: "#fff" }}>
       <h2>MCART E-Commerce</h2>
-      <p><b>Deployment ID:</b> XXXXX-XXXXX</p>
+      <p><b>Deployment ID:</b> 1234-5678</p>
     </div>
   );
 }
